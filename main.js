284d8b6486cd55ce959c7701e9fdf5d8b56b65da
Travis Fehr <travisefehr@gmail.com>
2022-05-24 09:52:31 -0700

    commit tree 97f65e9a871a95360a2d23714fed6ad5e3b9fe70

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+let modal = document.getElementById('modal').hidden
 
 
 //------------------------------------------------------------------------------
